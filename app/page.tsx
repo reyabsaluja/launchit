662b6929f41de47444902116ebc3eafba2de2e2a
ReyabSaluja <reyabsaluja@gmail.com>
2025-09-13 14:52:37 -0400

    QOL

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import CEOBriefForm from '@/components/CEOBriefForm';
+import OnboardingFlow from '@/components/OnboardingFlow';
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-background py-12 px-4 sm:px-6 lg:px-8">
-      <CEOBriefForm />
-    </div>
-  );
+  return <OnboardingFlow />;
 }
